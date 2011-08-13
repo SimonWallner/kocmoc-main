0ad1233781899fd6fa88c143e8c5c7032d50a076
Simon Wallner <me@simonwallner.at>
2011-08-13 10:12:55 +0200

    started to set up the build system

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream.h>
+
+int main(void)
+{
+	
+	std::cout	<< "//////////////////////////////////////////////////////////////////////////////" << std::endl
+	<< "   *           .                          .                          .        " << std::endl
+	<< "                             .                   .                         " << std::endl
+	<< "           .                                                    *           " << std::endl  
+	<< ".                                        .                               " << std::endl 
+	<< "                    .              .                  .                 .        " << std::endl
+	<< "                                                                      " << std::endl
+	<< "                                                                             " << std::endl
+	<< "         .           *                      .                 .                " << std::endl
+	<< "                               .           *                                   " << std::endl
+	<< "                                                                         .    " << std::endl
+	<< "                      .             .                         .                 " << std::endl
+	<< "*          .                                                                   " << std::endl
+	<< "                                                 .         0                   " << std::endl
+	<< "                             .                            /|\\                    " << std::endl
+	<< "          .                                                           .        " << std::endl
+	<< "                                                .                             " << std::endl
+	<< "                     *               .                    .                     " << std::endl
+	<< "   .                                                                 *         " << std::endl
+	<< "                                      .                                       " << std::endl
+	<< "//// kocmoc //////////////////////////////////////////////////////////////////" << std::endl;
+
+}
