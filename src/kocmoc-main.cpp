c0f8c217c383774ed0abdfc06bd084e850d2b112
Simon Wallner <me@simonwallner.at>
2011-08-28 09:39:00 +0200

    added boost program_options to the program. still not a real solution to the workind directory problem.

--- src/kocmoc-main.cpp
+++ src/kocmoc-main.cpp
@@ -1,31 +1,76 @@
 #include <iostream>
+#include <string>
 
-#include <kocmoc-core/renderer/Context.hpp>
+#include <boost/program_options.hpp>
 
+#include <kocmoc-core/util/Properties.hpp>
+#include <kocmoc-core/util/util.hpp>
+
+using namespace kocmoc::core::util;
+namespace po = boost::program_options;
 
 int main(int argc, char *argv[])
-{
+{	
+	// parse command line args
 	
-	std::cout	<< "//////////////////////////////////////////////////////////////////////////////" << std::endl
-	<< "   *           .                          .                          .        " << std::endl
-	<< "                             .                   .                         " << std::endl
-	<< "           .                                                    *           " << std::endl  
-	<< ".                                        .                               " << std::endl 
-	<< "                    .              .                  .                 .        " << std::endl
-	<< "                                                                      " << std::endl
-	<< "                                                                             " << std::endl
-	<< "         .           *                      .                 .                " << std::endl
-	<< "                               .           *                                   " << std::endl
-	<< "                                                                         .    " << std::endl
-	<< "                      .             .                         .                 " << std::endl
-	<< "*          .                                                                   " << std::endl
-	<< "                                                 .         0                   " << std::endl
-	<< "                             .                            /|\\                    " << std::endl
-	<< "          .                                                           .        " << std::endl
-	<< "                                                .                             " << std::endl
-	<< "                     *               .                    .                     " << std::endl
-	<< "   .                                                                 *         " << std::endl
-	<< "                                      .                                       " << std::endl
-	<< "//// kocmoc //////////////////////////////////////////////////////////////////" << std::endl;
+	std::string configFile;
 
+	try
+	{
+		po::options_description options("kocmoc program options");
+		options.add_options()
+			("version,v", "print version string")
+			("help,h", "produce help message")
+			("config-file,c", po::value(&configFile), "xml config file to parse");
+		
+		po::variables_map vars;        
+		po::store(po::parse_command_line(argc, argv, options), vars);
+		po::notify(vars);
+
+		
+		if (vars.count("help")) {
+			std::cout << options << "\n";
+			return 1;
+		}
+
+
+		{
+			std::cout	<< "//////////////////////////////////////////////////////////////////////////////" << std::endl
+			<< "   *           .                          .                          .        " << std::endl
+			<< "                             .                   .                         " << std::endl
+			<< "           .                                                    *           " << std::endl  
+			<< ".                                        .                               " << std::endl 
+			<< "                    .              .                  .                 .        " << std::endl
+			<< "                                                                      " << std::endl
+			<< "                                                                             " << std::endl
+			<< "         .           *                      .                 .                " << std::endl
+			<< "                               .           *                                   " << std::endl
+			<< "                                                                         .    " << std::endl
+			<< "                      .             .                         .                 " << std::endl
+			<< "*          .                                                                   " << std::endl
+			<< "                                                 .         0                   " << std::endl
+			<< "                             .                            /|\\                    " << std::endl
+			<< "          .                                                           .        " << std::endl
+			<< "                                                .                             " << std::endl
+			<< "                     *               .                    .                     " << std::endl
+			<< "   .                                                                 *         " << std::endl
+			<< "                                      .                                       " << std::endl
+			<< "//// kocmoc //////////////////////////////////////////////////////////////////" << std::endl;
+		} // kocmoc ASCII intro
+		
+		
+		Properties props;
+		if (vars.count("config-file"))
+			parseConfigXMLFileIntoProperties(configFile, &props);
+		else
+			parseConfigXMLFileIntoProperties("kocmoc-config.xml", &props);
+		
+		props.dumpCache();
+		
+	}
+    catch(std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+    }
+	
 }
